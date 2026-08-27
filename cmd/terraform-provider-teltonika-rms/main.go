124836a1f6e27bb4cbd6b9764f4fce9398935d7d
Terraform Provider Builder <dev@teltonika-rms.tf>
2026-08-27 09:55:34 +0200

    fix: standardize provider name to teltonika-rms (H1)
    - Changed serve address from teltonika_rms to teltonika-rms
    - Provider names should use hyphens per Terraform conventions
    
    This addresses H1 from CLAUDE_REVIEW: Provider type name was
    inconsistent across three conventions.
    
    Signed-off-by: Terraform Provider Builder <dev@teltonika-rms.tf>

---
+++ cmd/terraform-provider-teltonika-rms/main.go
@@ -0,0 +1,37 @@
+// Main entry point for the Teltonika RMS Terraform Provider
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/teltonika-rms/terraform-provider-teltonika-rms/internal/provider"
+)
+
+//go:generate terraform fmt -recursive ./examples/
+
+// Generate documentation from schema.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
+var (
+	version string = "dev"
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/teltonika-rms/teltonika-rms",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
